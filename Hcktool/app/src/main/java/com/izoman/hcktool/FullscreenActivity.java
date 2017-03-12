b9b8d9d1ca8fa51355af2440bb23243d80a38e60
Izoman <umut.saglam1@hotmail.com>
2017-03-12 22:32:11 +0100

    Now true full screen app + added basic time

--- Hcktool/app/src/main/java/com/izoman/hcktool/FullscreenActivity.java
+++ Hcktool/app/src/main/java/com/izoman/hcktool/FullscreenActivity.java
@@ -1,34 +1,26 @@
 package com.izoman.hcktool;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
 
 /**
  * Main view
  */
 public class FullscreenActivity extends AppCompatActivity {
-    private View mContentView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_fullscreen);
-        mContentView = findViewById(R.id.fullscreen_content);
-        hide();
     }
 
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    @Override
+    protected void onResume(){
+        super.onResume();
     }
 }
