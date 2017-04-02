e3b5ca40676b75674d7e9cf837be662d79d5c614
Umut Saglam <umut.saglam@student.odisee.be>
2017-04-02 17:01:40 +0200

    Added new font (better ui) + background + 2 new buttons

--- Hcktool/app/src/main/java/com/izoman/hcktool/FullscreenActivity.java
+++ Hcktool/app/src/main/java/com/izoman/hcktool/FullscreenActivity.java
@@ -1,9 +1,12 @@
 package com.izoman.hcktool;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -17,6 +20,13 @@ public class FullscreenActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_fullscreen);
+        // Set title font hacked
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/HACKED.ttf");
+        ((TextView)findViewById(R.id.textViewTitle)).setTypeface(custom_font);
+        ((TextView)findViewById(R.id.textClock)).setTypeface(custom_font);
+        ((Button)findViewById(R.id.buttonStart)).setTypeface(custom_font);
+        ((Button)findViewById(R.id.buttonAbout)).setTypeface(custom_font);
+
     }
 
     @Override
