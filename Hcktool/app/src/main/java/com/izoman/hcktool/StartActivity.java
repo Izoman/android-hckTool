1180a3eb74fe4e6244be5c23e085eb9547edcc77
Umut Saglam <umut.saglam@student.odisee.be>
2017-04-05 14:37:27 +0200

    Added working filter on StartActivity

--- Hcktool/app/src/main/java/com/izoman/hcktool/StartActivity.java
+++ Hcktool/app/src/main/java/com/izoman/hcktool/StartActivity.java
@@ -2,15 +2,21 @@ package com.izoman.hcktool;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Iterator;
 
 
 /**
@@ -18,6 +24,8 @@ import android.widget.Toast;
  */
 public class StartActivity extends AppCompatActivity  {
     Context context;
+    LinearLayout toolsContainer;
+    int bgColorRed, bgColorGreen, bgColorBlue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +40,48 @@ public class StartActivity extends AppCompatActivity  {
         ((TextView)findViewById(R.id.textClock)).setTypeface(custom_font);
         ((Button)findViewById(R.id.buttonBack)).setTypeface(custom_font);
         context = this.getApplicationContext();
+        bgColorRed = ContextCompat.getColor(context, R.color.colorHackingRed);
+        bgColorGreen = ContextCompat.getColor(context, R.color.colorHackingGreen);
+        bgColorBlue = ContextCompat.getColor(context, R.color.colorHackingBlue);
+        toolsContainer = (LinearLayout) findViewById(R.id.tools_container);
     }
-    public void buttonClicked(View view) {
-        if (view.getId() == R.id.buttonStart) {
 
-        } else if (view.getId() == R.id.buttonBack) {
+    public void buttonClicked(View view) {
+        if (view.getId() == R.id.buttonFilterBeginner) {
+            // bgColorGreen = beginner
+            filterList(bgColorGreen);
+        } else if (view.getId() == R.id.buttonFilterIntermediate) {
+            // bgColorBlue = intermediate
+            filterList(bgColorBlue);
+        }
+        else if (view.getId() == R.id.buttonFilterExpert) {
+            // bgColorRed = expert
+            filterList(bgColorRed);
+        }
+        else if(view.getId() == R.id.buttonFilterAll) {
+            // All
+            filterList(0);
+        }
+        else if (view.getId() == R.id.buttonBack) {
             this.finish();
+        }
+    }
+
+    /**
+     * Filters hacking tool list according to background color
+     * When given 0, everything is shown = no filter
+     * @param bgColor
+     */
+    protected void filterList(int bgColor) {
+        for (int i = 0; i < toolsContainer.getChildCount(); i++) {
+            View v = toolsContainer.getChildAt(i);
+            if (v instanceof Button) {
+                ColorDrawable buttonColor = (ColorDrawable) v.getBackground();
+                if(bgColor != 0) {
+                    if(buttonColor.getColor() == bgColor) v.setVisibility(View.VISIBLE);
+                    else v.setVisibility(View.GONE);
+                } else v.setVisibility(View.VISIBLE);
+            }
         }
     }
 
